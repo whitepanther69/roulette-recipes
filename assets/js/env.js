0c6f1d49c1c5f74d5810b45320e203fffa2c1c52
whitepanther69 <159169210+whitepanther69@users.noreply.github.com>
2024-05-03 16:05:41 +0100

    Update env.js

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,6 +1,7 @@
 // env.js
-//const API_KEY = "617fd74bc7b7467fbba5974ee312c9f2";
+// const API_KEY = "617fd74bc7b7467fbba5974ee312c9f2";
 // const API_KEY = "057fd430bdb148268229c6a3b69b0f12";
-const API_KEY = "3bb4f394cf0644d2b9d4dffe66bbcfd1";
+// const API_KEY = "3bb4f394cf0644d2b9d4dffe66bbcfd1";
+   const API_KEY = "cbe248010892413d910669f2ecb2957e";
 
 // export { API_KEY, API_URL };
